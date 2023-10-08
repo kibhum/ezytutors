c51d7e6e2b1e1a0e94d55d97a8aab095944174fc
kibethh <kibethh@gmail.com>
2023-10-08 20:51:15 +0300

    Added new course route

--- tutor-nodb/src/bin/tutor-service.rs
+++ tutor-nodb/src/bin/tutor-service.rs
@@ -11,6 +11,9 @@ mod routes;
 #[path = "../state.rs"]
 mod state;
 
+#[path = "../models.rs"]
+mod models;
+
 use routes::*;
 use state::AppState;
 
@@ -19,11 +22,13 @@ async fn main() -> io::Result<()> {
     let shared_data = web::Data::new(AppState {
         health_check_response: "I'm good. You've already asked me ".to_string(),
         visit_count: Mutex::new(0),
+        courses: Mutex::new(vec![]),
     });
     let app = move || {
         App::new()
             .app_data(shared_data.clone())
             .configure(general_routes)
+            .configure(course_routes)
     };
     HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
 }
